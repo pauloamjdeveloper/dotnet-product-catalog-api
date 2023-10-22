453defe9e72fcb64534884bb0e26f9124c3af15a
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-10-22 12:22:17 -0300

    Criando as classes de DTOs e seus mapeamentos

---
+++ src/ProductCatalog.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using ProductCatalog.Application.DTOs;
+using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
