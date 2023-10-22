58c225c477b473e226f176a3eb4d60eca8cc2c81
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-10-22 14:05:25 -0300

    Criando migration e adicionando imagens

---
+++ src/ProductCatalog.Infra.Data/Migrations/20231022165338_SeedProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCatalog.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+            "VALUES('Caderno espiral','Caderno espiral 100 fôlhas',7.45,50,'caderno-1.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+            "VALUES('Chaveiro Chip','Chaveiro personalizado em formato chip',30.90,30,'chaveiro.png',3)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+            "VALUES('Borracha escolar','Borracha branca pequena',3.25,80,'borracha.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+            "VALUES('Calculadora escolar','Calculadora simples',15.39,20,'calculadora.png',2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
